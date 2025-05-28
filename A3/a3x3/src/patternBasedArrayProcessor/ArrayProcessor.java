ddd66d133c0dc19512cefd0938e981e7cc5a84cc
Dylan <dylan.dagomber@gmail.com>
2025-05-28 11:35:59 +0200

    Updatet a3x3

--- A3/a3x3/src/patternBasedArrayProcessor/ArrayProcessor.java
+++ A3/a3x3/src/patternBasedArrayProcessor/ArrayProcessor.java
@@ -1,62 +1,65 @@
 // This source code is UTF-8 coded - see https:/stackoverflow.com/questions/9180981/how-to-support-utf-8-encoding-in-eclipse
 package patternBasedArrayProcessor;
 
-
 /**
  * ArrayProcessor - see task
- * 
- * @author   (your name(s)) 
- * @version  (a version number or a date)
+ *
+ * @author (your name(s))
+ * @version (a version number or a date)
  *
  */
 public class ArrayProcessor {
-    
+
 	int summe = 0;
 	int patternHight = 4;
 	int startPosition = 0;
-	int y = 0;
+
 	;
-	
-	public long process( long[][] theArray ){
-		
-		boolean[][]  pattern=  {
-	    		 {false,true,true},
-	    		 {true,false,false,true},
-	    		 {true,false,false,true},
-	    		 {false,true,true},
-	    };
-		
-		int row=0;
-     	while(theArray.length>=patternHight+row) { 
-    	 		if(theArray[row].length-1 >= pattern[0].length-1 &&
-    	 			theArray[row+1].length-1 >= pattern[1].length-1 &&
-    	 			theArray[row+2].length-1 >= pattern[2].length-1 &&
-    	 			theArray[row+3].length-1 >= pattern[3].length-1) {
-    	 		
-    	 			for(int x=0;x<theArray.length-patternHight;) {
-    	 		  
-    	 				summe+=theArray[y][x+1]+theArray[y][x+2]+
-    	 	    		   theArray[y+1][x]+theArray[y][x+3]+
-    	 	    		   theArray[y+2][x]+theArray[y][x+3]+
-    	 	    		   theArray[y+3][x+1]+theArray[y][x+2];
-    	 					x++;
-    	 			}//
-    	 				
-    	 		}/*if */ 		else {
-    	 								row++;
-    	 								y++;	
-    	 						}//else
-    	 							
-    	 									row++;
-    	 }//while
-     										return summe;   
-     
-        // <<<=== Diese Zeile ist falsch!   bzw. nur ein Platzhalter um den Compiler gluecklich zu machen. Die Zeile muss sinnvoll ersetzt werden.
-    }//method()
-   
-    
-    //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-    // Führen Sie Änderungen oberhalb dieser Zeilen durch.
-    //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-    
-}//class
+
+	public long process(long[][] theArray) {
+
+		boolean[][] pattern = {
+				{ false, true, true },
+				{ true, false, false, true },
+				{ true, false, false, true },
+				{ false, true, true }, };
+
+		int row = 0;
+		int x = 0;
+		while (theArray.length >= patternHight + row) {
+
+			// solange das pattern passt
+			// summe addieren
+			// und nach rechts verschieben
+
+			if (theArray[row].length - 1 >= pattern[0].length - 1 + x
+					&& theArray[row + 1].length - 1 >= pattern[1].length - 1 + x
+					&& theArray[row + 2].length - 1 >= pattern[2].length - 1 + x
+					&& theArray[row + 3].length - 1 >= pattern[3].length - 1 + x) {
+
+				// for(int x=0;x<theArray.length-patternHight;) {
+
+				summe += theArray[row][x + 1] + theArray[row][x + 2] + theArray[row + 1][x] + theArray[row][x + 3]
+						+ theArray[row + 2][x] + theArray[row][x + 3] + theArray[row + 3][x + 1] + theArray[row][x + 2];
+				x++;
+				// }//for
+
+			} // if
+			else {
+				row++;
+				y++;
+			} // else
+
+			//row++;
+		} // while
+		return summe;
+
+		// <<<=== Diese Zeile ist falsch! bzw. nur ein Platzhalter um den Compiler
+		// gluecklich zu machen. Die Zeile muss sinnvoll ersetzt werden.
+	}// method()
+
+	// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+	// Führen Sie Änderungen oberhalb dieser Zeilen durch.
+	// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+}// class
